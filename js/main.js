63a17e3162445c7ee41f1710487cd07b4f90c3cc
Luisa-G <luisa.gonzalezpico@gmail.com>
2025-01-18 14:07:56 -0600

    dos puntos de nosotros

--- js/main.js
+++ js/main.js
@@ -5,3 +5,39 @@ document.querySelector('.nav-link.dropdown-toggle').addEventListener('click', fu
       this.nextElementSibling.classList.toggle('show'); // Cambia el menú desplegable
   }
 });
+
+
+
+//para el acordión
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     }
+//   });
+// }
+
+var acc = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            // Si ya está abierto, colapsa el panel
+            panel.style.maxHeight = null;
+        } else {
+            // Ajusta la altura máxima al tamaño del contenido del panel
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
