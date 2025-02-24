bffb5385dde7f72ad526917a50bcc209fcbe3969
Luisa-G <luisa.gonzalezpico@gmail.com>
2025-02-23 18:50:20 -0600

    con barra de navegación funcional

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-document.querySelector('.nav-link.dropdown-toggle').addEventListener('click', function(e) {
-  if (!this.parentElement.classList.contains('show')) {
-      e.preventDefault(); // Evita la redirección si el dropdown no está visible
-      this.parentElement.classList.toggle('show'); // Muestra/oculta el dropdown
-      this.nextElementSibling.classList.toggle('show'); // Cambia el menú desplegable
-  }
-});
 
 
-
-//para el acordión
+//para el acordeón
 var acc = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < acc.length; i++) {
